d5f3f4a1d8049881542657db69f58939f05516ce
MarianneTeixido <ztp.txdo@gmail.com>
2023-11-03 18:39:38 -0600

    fix:visualization movies

--- cinecity/src/components/Movies/Movies.tsx
+++ cinecity/src/components/Movies/Movies.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import "./Movies.css"
+import { Movie } from '../../services/types';
+
+
 function Movies() {
     console.log('renderizando movies')
     //movies es la data
@@ -8,7 +11,6 @@ function Movies() {
 
     //Permite hacer la petición HTTP
     useEffect(() => {
-    
         const options = {
             method: 'GET',
             headers: {
@@ -16,37 +18,27 @@ function Movies() {
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmVkNTM4ZjM1ZGM1ZTg1YjQ5ZmIxNzY0ZWVjOWFiNyIsInN1YiI6IjY1MzE1NWQ3YWVkZTU5MDEyYjMxOTEzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ofkUDzjvsajBgmO4rJ78Sea0ge-s5fhc5g8abkDBGSo'
             }
         };
-        // fetch("https://api.themoviedb.org/3/genre/movie/list?language=en", options)
-        fetch("https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc", options)
+        fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc", options)
             .then((response) => response.json())
             .then((movies) => setMovies(movies.results));
     }, []);
- 
+
     return (
 
-        <div  >
-            {movies ? (<ul className='grid-movies' >{movies.map((results) =>
-                <li>
-                    {results.title}
-                </li>
-            )}</ul>) : 'no hay película'}
-        </div>
+        <section className='container-movies'  >
+            {movies.map((movie: Movie, i: number) =>
+                <div className='movie-item' key={i}>
+                    <img className='img' src={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`} />
+                    <h3>{movie.title}</h3>
+                    <p>{new Date(movie.release_date).getFullYear()}</p>
+                </div>
+            )}
+        </section>
     )
 }
 
 export default Movies;
 
-
-// return (
-
-//     <div  >
-//         {movies ? (<ul className='grid-movies' >{movies.map((results) =>
-//             <li>
-//                 {results.title}
-//             </li>
-//         )}</ul>) : 'no hay película'}
-//     </div>
-// )
 
 // import { Movie } from "../../services/movie-request";
 // import "./Movies.css";
--- cinecity/src/services/movie-request.ts
+++ cinecity/src/services/movie-request.ts
@@ -1,18 +1,18 @@
-import {Movie} from "./types"
+// import {Movie} from "./types"
 
-export const getMovies = (): Promise<Movie[]> =>
-  new Promise((resolve, reject) => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmVkNTM4ZjM1ZGM1ZTg1YjQ5ZmIxNzY0ZWVjOWFiNyIsInN1YiI6IjY1MzE1NWQ3YWVkZTU5MDEyYjMxOTEzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ofkUDzjvsajBgmO4rJ78Sea0ge-s5fhc5g8abkDBGSo'
-      }
-    };
+// export const getMovies = (): Promise<Movie[]> =>
+//   new Promise((resolve, reject) => {
+//     const options = {
+//       method: 'GET',
+//       headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmVkNTM4ZjM1ZGM1ZTg1YjQ5ZmIxNzY0ZWVjOWFiNyIsInN1YiI6IjY1MzE1NWQ3YWVkZTU5MDEyYjMxOTEzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ofkUDzjvsajBgmO4rJ78Sea0ge-s5fhc5g8abkDBGSo'
+//       }
+//     };
 
-    fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
-      .then((response) => response.json())
-      .then((response) => resolve(response?.results))
-      .catch(err => reject(err)); 
-  })
+//     fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+//       .then((response) => response.json())
+//       .then((response) => resolve(response?.results))
+//       .catch(err => reject(err)); 
+//   })
  
